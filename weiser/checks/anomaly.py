cf62e929e85caa1974fc89688c06e8f9038f78d0
Paco Valdez <paco@cube.dev>
2024-10-29 15:43:59 -0700

    Add grouby in anomaly check

--- weiser/checks/anomaly.py
+++ weiser/checks/anomaly.py
@@ -24,43 +24,80 @@ class CheckAnomaly(BaseCheck):
         for dataset in datasets:
             exp = self.parse_dataset(dataset)
             q = self.get_query(exp, verbose)
-            result_window = self.execute_query(q, verbose)
-            if len(result_window) < 5:
-                self.append_result(
-                    False,
-                    result_window[-1][0] if len(result_window) > 0 else None,
-                    results,
-                    dataset,
-                    datetime.now(),
-                    verbose,
+            result_windows = self.execute_query(q, verbose)
+            if self.check.dimensions:
+                dim = self.check.dimensions[0]
+                all_results = pd.DataFrame(
+                    result_windows, columns=[dim, "actual_value", "run_time"]
                 )
-                continue
-            results_df = pd.DataFrame(
-                result_window, columns=["actual_value", "run_time"]
-            )
-            with duckdb.connect(":memory:") as conn:
-                conn.execute("CREATE TABLE results_table AS SELECT * FROM results_df")
+                unique_dim_values = all_results[dim].unique()
+                result_windows = []
+                for dim_value in unique_dim_values:
+                    all_results_dim = all_results[all_results[dim] == dim_value]
+                    result_windows.append(
+                        (
+                            dim_value,
+                            all_results_dim[
+                                ["actual_value", "run_time"]
+                            ].values.tolist(),
+                        )
+                    )
 
-                rows = conn.execute(
-                    """ SELECT mad(actual_value), median(actual_value), last(actual_value) 
-                        FROM (SELECT * FROM results_df ORDER BY run_time ASC) q LIMIT 1"""
-                ).fetchall()
-            # Algorithm Name: Median Absolute Deviation (MAD)
-            # M_i = 0.6745 * (x_i - Median(X) ) / MAD
-            # Robust Z-score formula.
-            # 0.6745 is the 75th percentile of the standard normal distribution
-            # to which the MAD converges to.
-            # If MAD -> 0 then Z score is 0 for testing purposes.
-            # (Constant value across time, last_value = Median with std = 0)
-            m_i = (
-                (0.6745 * (rows[0][2] - rows[0][1]) / (rows[0][0]))
-                if int(rows[0][0]) != 0
-                else 0
-            )
-            success = self.apply_condition(m_i)
-            self.append_result(
-                success, rows[0][2], results, dataset, datetime.now(), verbose
-            )
+            else:
+                result_windows = [(None, result_windows)]
+
+            for dim_value, result_window in result_windows:
+
+                results_df = pd.DataFrame(
+                    result_window, columns=["actual_value", "run_time"]
+                )
+
+                if len(result_window) < 5:
+                    self.append_result(
+                        False,
+                        result_window[-1][0] if len(result_window) > 0 else None,
+                        results,
+                        dataset,
+                        datetime.now(),
+                        verbose,
+                    )
+                    continue
+
+                with duckdb.connect(":memory:") as conn:
+                    conn.execute(
+                        "CREATE TABLE results_table AS SELECT * FROM results_df"
+                    )
+
+                    rows = conn.execute(
+                        """ SELECT mad(actual_value), median(actual_value), last(actual_value) 
+                            FROM (SELECT * FROM results_df ORDER BY run_time ASC) q LIMIT 1"""
+                    ).fetchall()
+                # Algorithm Name: Median Absolute Deviation (MAD)
+                # M_i = 0.6745 * (x_i - Median(X) ) / MAD
+                # Robust Z-score formula.
+                # 0.6745 is the 75th percentile of the standard normal distribution
+                # to which the MAD converges to.
+                # If MAD -> 0 then Z score is 0 for testing purposes.
+                # (Constant value across time, last_value = Median with std = 0)
+                m_i = (
+                    (0.6745 * (rows[0][2] - rows[0][1]) / (rows[0][0]))
+                    if int(rows[0][0]) != 0
+                    else 0
+                )
+                success = self.apply_condition(m_i)
+                if dim_value:
+                    self.append_result(
+                        success,
+                        [dim_value, rows[0][2]],
+                        results,
+                        dataset,
+                        datetime.now(),
+                        verbose,
+                    )
+                else:
+                    self.append_result(
+                        success, rows[0][2], results, dataset, datetime.now(), verbose
+                    )
 
         return results
 
@@ -79,6 +116,9 @@ class CheckAnomaly(BaseCheck):
         limit: int = 100,
         verbose: bool = False,
     ) -> Select:
+        if self.check.dimensions:
+            dim = self.check.dimensions[0]
+            select_stmnt = [dim] + select_stmnt
         q = (
             Select()
             .from_(table)
